6326942e9184abad842a33e419f6566dcdb14a2d
Squirblechem <152367545+jman2476@users.noreply.github.com>
2024-01-17 14:36:37 -0500

    gets user input, creates an svg logo based on the input also added a folder that it writes the files to, and each file is named based on the time of creation and internal logo text

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
 // bring in the shapes
 const [Shape, Triangle, Circle, Square] = require('./lib/shapes')
-
+// bring in inquirer and fs, and dayjs for naming files
 const inquirer = require("inquirer")
+const fs = require('fs')
+const dayjs = require('dayjs')
 
-const triMan = new Triangle()
 
-console.log(triMan.renderSVG())
+
+inquirer.prompt([{
+    type: 'list',
+    name: 'shape',
+    message: 'What shape logo would you like?',
+    choices: ['Triangle', 'Circle', 'Square']
+},
+{
+    type: 'input',
+    name: 'fillColor',
+    message: 'What color should the shape be? Use a color keyword, or a hexdecimal preceded by #'
+},
+{
+    type: 'input',
+    name: 'text',
+    message: 'What text do you want on your logo? Only the first 3 characters will be used'   
+},
+{
+    type: 'input',
+    name: 'textColor',
+    message: 'What color should the text be? Use a color keyword, or a hexdecimal preceded by #'
+}]).then((answerObj) => {
+
+    switch(answerObj.shape) {
+        case 'Triangle':
+            const triangle  = new Triangle()
+            triangle.setColor(answerObj.fillColor)
+            triangle.setText(answerObj.text)
+            triangle.setTextColor(answerObj.textColor)
+            return triangle
+        case 'Circle':
+            const circle  = new Circle()
+            circle.setColor(answerObj.fillColor)
+            circle.setText(answerObj.text)
+            circle.setTextColor(answerObj.textColor)
+            return circle
+        case 'Square':
+            const square  = new Square()
+            square.setColor(answerObj.fillColor)
+            square.setText(answerObj.text)
+            square.setTextColor(answerObj.textColor)
+            return square
+    }
+}).then((logoObj) => {
+    const dateTag = dayjs().format('D-M-YY_hh-mm')
+    fs.writeFile(`./logos/${logoObj.text}-Logo-${dateTag}.svg`, logoObj.renderSVG(), (err => {
+        if (err) return console.log(err)
+
+        console.log('Successfully made new logo!')
+    }))
+})
